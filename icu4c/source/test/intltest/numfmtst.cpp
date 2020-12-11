7f80564e5a3562f2d9cdce753059021c24650676
gnrunge <nrunge@google.com>
2020-12-10 16:27:37 -0800

    Fix unittest again, extend CI script.

--- icu4c/source/test/intltest/numfmtst.cpp
+++ icu4c/source/test/intltest/numfmtst.cpp
@@ -674,7 +674,7 @@ NumberFormatTest::TestScientificGrouping() {
     UErrorCode status = U_ZERO_ERROR;
     DecimalFormat fmt("##0.00E0",status);
     if (assertSuccess("", status, true, __FILE__, __LINE__)) {
-        expect(fmt, .01234, "10.3E-3");
+        expect(fmt, .01234, "12.3E-3");
         expect(fmt, .1234, "123E-3");
         expect(fmt, 1.234, "1.23E0");
         expect(fmt, 12.34, "12.3E0");
